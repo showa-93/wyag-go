aecc402dc8beee1642d9dba4f9e1304a2fe87120
showa-93 <shumai.1215.2018@gmail.com>
2022-03-24 03:31:03 +0900

    cat-file コマンドを追加

--- main.go
+++ main.go
@@ -7,6 +7,11 @@ import (
 	"path/filepath"
 )
 
+var (
+	BasePath          = "."
+	Types    []string = []string{"blob", "commit", "tag", "tree"}
+)
+
 type Command interface {
 	Run() error
 }
@@ -48,16 +53,77 @@ func (i *Init) Run() error {
 	return err
 }
 
+type CatFile struct {
+	*flag.FlagSet
+	Type   string
+	Object string
+}
+
+func NewCatFile(args []string) *CatFile {
+	cf := &CatFile{}
+	cf.FlagSet = flag.NewFlagSet("cat-file", flag.ExitOnError)
+	cf.Usage = func() {
+		o := flag.CommandLine.Output()
+		fmt.Fprint(o, "Usage: cat-file [TYPE] [OBJECT]\n")
+		fmt.Fprint(o, "\tProvide content of repository objects.\n")
+	}
+
+	cf.Parse(args)
+	if len(cf.Args()) != 2 {
+		fmt.Printf("expected 1 arguments count=%d\n", len(cf.Args()))
+		os.Exit(1)
+	}
+	cf.Type = cf.Args()[0]
+	cf.Object = cf.Args()[1]
+
+	for _, t := range Types {
+		if t == cf.Type {
+			return cf
+		}
+	}
+
+	fmt.Printf("unknown object type %s\n", cf.Type)
+	os.Exit(1)
+	return nil
+}
+
+func (cf *CatFile) Run() error {
+	repo, err := FindRepository(BasePath, true)
+	if err != nil {
+		return err
+	}
+	o, err := ReadObject(repo, cf.Object)
+	if err != nil {
+		return err
+	}
+	b, err := o.Serialize()
+	if err != nil {
+		return err
+	}
+	fmt.Println(string(b))
+	return nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("expected subcommands")
 		os.Exit(1)
 	}
 
+	if path := os.Getenv("WYAG_WORKDIR"); path != "" {
+		BasePath = path
+	}
+
 	switch os.Args[1] {
 	case "init":
 		i := NewInit(os.Args[2:])
 		if err := i.Run(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+	case "cat-file":
+		cf := NewCatFile(os.Args[2:])
+		if err := cf.Run(); err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
